d059abc4ac76e6810eea82360d8692f1a63e3fd6
Luyao <smmileely@gmail.com>
2022-08-31 13:30:53 +0800

    exercise 4 testing login form solution 1

--- src/__tests__/exercise/04.js
+++ src/__tests__/exercise/04.js
@@ -6,14 +6,29 @@ import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Login from '../../components/login'
 
-test('submitting the form calls onSubmit with username and password', () => {
+test('submitting the form calls onSubmit with username and password', async () => {
   // 🐨 create a variable called "submittedData" and a handleSubmit function that
+  let submittedData
   // accepts the data and assigns submittedData to the data that was submitted
   // 💰 if you need a hand, here's what the handleSubmit function should do:
-  // const handleSubmit = data => (submittedData = data)
+  const handleSubmit = data => (submittedData = data)
   //
   // 🐨 render the login with your handleSubmit function as the onSubmit prop
   //
+  // render login component
+  render(<Login onSubmit={handleSubmit} />)
+  const username = 'chucknorris'
+  const password = 'i need no password'
+
+  // get from testing playground in chrome extension
+  // screen.getByRole('textbox', {name: /username/i})
+  await userEvent.type(screen.getByLabelText(/username/i), username)
+  await userEvent.type(screen.getByLabelText(/password/i), password)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  // know how the ui looks like right after log in
+  // screen.debug()
+
   // 🐨 get the username and password fields via `getByLabelText`
   // 🐨 use `await userEvent.type...` to change the username and password fields to
   //    whatever you want
@@ -22,6 +37,10 @@ test('submitting the form calls onSubmit with username and password', () => {
   //
   // assert that submittedData is correct
   // 💰 use `toEqual` from Jest: 📜 https://jestjs.io/docs/en/expect#toequalvalue
+  expect(submittedData).toEqual({
+    username,
+    password,
+  })
 })
 
 /*
