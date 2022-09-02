07b3b0acfb330299714131188014c3c8e00a7868
Luyao <smmileely@gmail.com>
2022-09-02 18:19:16 +0800

    exercise 7 bonus 2 custom render method

--- src/__tests__/exercise/07.js
+++ src/__tests__/exercise/07.js
@@ -6,31 +6,27 @@ import {render, screen} from '@testing-library/react'
 import {ThemeProvider} from '../../components/theme'
 import EasyButton from '../../components/easy-button'
 
-test('renders with the light styles for the light theme', () => {
-  // 🐨 uncomment all of this code and your test will be busted on the next line:
+function renderWithTheme(ui, {theme = 'light', ...options} = {}) {
   const Wrapper = ({children}) => (
-    <ThemeProvider initialTheme="light">{children}</ThemeProvider>
+    <ThemeProvider initialTheme={theme}>{children}</ThemeProvider>
   )
 
-  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  render(ui, {wrapper: Wrapper, ...options})
+}
+
+test('renders with the light styles for the light theme', () => {
+  renderWithTheme(<EasyButton>Easy</EasyButton>)
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: white;
     color: black;
   `)
-  //
-  // 🐨 update the `render` call above to use the wrapper option using the
-  // ThemeProvider
 })
 
 /* eslint no-unused-vars:0 */
 // to test dark theme
 test('renders with the light styles for the dark theme', () => {
-  const Wrapper = ({children}) => (
-    <ThemeProvider initialTheme="dark">{children}</ThemeProvider>
-  )
-
-  render(<EasyButton>Easy</EasyButton>, {wrapper: Wrapper})
+  renderWithTheme(<EasyButton>Easy</EasyButton>, {theme: 'dark'})
   const button = screen.getByRole('button', {name: /easy/i})
   expect(button).toHaveStyle(`
     background-color: black;
